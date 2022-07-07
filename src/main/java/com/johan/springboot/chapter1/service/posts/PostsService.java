e64055c7c51f4076e43456d426cf63c7c1a52c94
Johan <Johan1102@github.com>
2022-07-07 18:05:22 +0900

    Spring Data JPA 등록 API 만들기

---
+++ src/main/java/com/johan/springboot/chapter1/service/posts/PostsService.java
@@ -0,0 +1,18 @@
+package com.johan.springboot.chapter1.service.posts;
+
+import com.johan.springboot.chapter1.domain.posts.PostsRepository;
+import com.johan.springboot.chapter1.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
