2a4c934646d151342d1e3d55327854321548ce58
Johan <Johan1102@github.com>
2022-07-08 17:56:03 +0900

    머스테치 화면 구성 테스트

---
+++ src/main/java/com/johan/springboot/chapter1/web/indexController.java
@@ -0,0 +1,15 @@
+package com.johan.springboot.chapter1.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+//머스테치 스타터 덕분에 컨트롤러에서 문자열 반환 시 앞의 경로(\src\main\resources\templates)와
+//뒤의 파일 확장자(.mustache)가 자동으로 지정
+@Controller
+public class indexController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+}
