438335fe6b13db4b23e93d055d935ea495ad52cc
Johan <Johan1102@github.com>
2022-07-25 16:24:33 +0900

    게시글 등록 화면 만들기

--- src/main/java/com/johan/springboot/chapter1/web/indexController.java
+++ src/main/java/com/johan/springboot/chapter1/web/indexController.java
@@ -4,12 +4,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 //머스테치 스타터 덕분에 컨트롤러에서 문자열 반환 시 앞의 경로(\src\main\resources\templates)와
-//뒤의 파일 확장자(.mustache)가 자동으로 지정
+//뒤의 파일 확장자(.mustache)가 자동으로 지정 (src\main\resources\templates\index.mustache)
 @Controller
 public class indexController {
 
     @GetMapping("/")
     public String index() {
         return "index";
+    }
+
+    @GetMapping("/posts/save")
+    public String postsSave() {
+        return "posts-save";
     }
 }
