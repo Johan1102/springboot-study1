95b5c1e97c9aedb49004fca03a172b714777b8bd
Johan <Johan1102@github.com>
2022-08-02 18:14:02 +0900

    목록 출력 기능 추가

--- src/main/java/com/johan/springboot/chapter1/service/posts/PostsService.java
+++ src/main/java/com/johan/springboot/chapter1/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.johan.springboot.chapter1.service.posts;
 
 import com.johan.springboot.chapter1.domain.posts.Posts;
 import com.johan.springboot.chapter1.domain.posts.PostsRepository;
+import com.johan.springboot.chapter1.web.dto.PostsListResponseDto;
 import com.johan.springboot.chapter1.web.dto.PostsResponseDto;
 import com.johan.springboot.chapter1.web.dto.PostsSaveRequestDto;
 import com.johan.springboot.chapter1.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -33,5 +37,13 @@ public class PostsService {
         Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id = "+id));
 
         return new PostsResponseDto(entity);
+    }
+
+    //readOnly 조건 추가 시 트랜잭션 범위는 유지하되 조회 기능만 남겨두어 속도를 개선, CUD 기능이 필요없는 서비스의 경우 사용
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        //.map(PostsResponseDto::new) == .map(posts -> new PostsResponseDto(posts)), 람다식 표현
+        return postsRepository.findAllDesc().stream().map(PostsListResponseDto::new).collect(Collectors.toList());
+//                postsRepository.findAllDesc().stream().map(PostsResponseDto::new).collect(Collectors.toList());
     }
 }
--- src/main/java/com/johan/springboot/chapter1/web/indexController.java
+++ src/main/java/com/johan/springboot/chapter1/web/indexController.java
@@ -1,15 +1,26 @@
 package com.johan.springboot.chapter1.web;
 
+import com.johan.springboot.chapter1.service.posts.PostsService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+
+@RequiredArgsConstructor
 //머스테치 스타터 덕분에 컨트롤러에서 문자열 반환 시 앞의 경로(\src\main\resources\templates)와
 //뒤의 파일 확장자(.mustache)가 자동으로 지정 (src\main\resources\templates\index.mustache)
 @Controller
 public class indexController {
 
+    private final PostsService postsService;
+
     @GetMapping("/")
-    public String index() {
+    public String index(Model model)
+    {
+        //Model : 서버 템플릿 엔진에서 사용할 수 있는 객체 저장 가능
+        //postsService.findAllDesc()로 가져온 결과를 posts로 index.mustache에 전달
+        model.addAttribute("posts", postsService.findAllDesc());
         return "index";
     }
 
